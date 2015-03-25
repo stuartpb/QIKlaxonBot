383f5917e89e032e6e7fc47ca18c08af88e7da94
Stuart P. Bentley <stuart@testtrack4.com>
2015-03-25 20:42:23 +0000

    New user agent

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var pkg = require('./package.json');
 var rereddit = require('rereddit');
 
-rereddit.user_agent = "StuartPBentley-QIKlaxonBot/" + pkg.version;
+rereddit.user_agent = "QIKlaxonBot/" + pkg.version + '(by /u/StuartPBentley)';
 
 var asUser;
 
@@ -17,13 +17,13 @@ rereddit.login(process.env.REDDIT_USERNAME, process.env.REDDIT_PASSWORD)
   .end(function(err, user) { asUser = user });
 
 function parseForNewComments(err,inbox) {
-  
+
 }
 
 function queryReddit() {
   if (asUser){
     if(commentsToCheck){
-      
+
     } else {
       rereddit.as(asUser).inbox(parseForNewComments);
     }
