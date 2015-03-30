1a051fe4a71d794da71ecd55f91cdd345b50e880
Stuart P. Bentley <stuart@testtrack4.com>
2015-03-29 20:02:50 -0700

    Deauth before hitting the DB

--- index.js
+++ index.js
@@ -30,28 +30,28 @@ module.exports = function appctor(cfg) {
     userAuthReddit.auth(req.query.code).then(function (refreshToken){
       return userAuthReddit('/api/v1/me').get();
     }).then(function(data){
-      return r.table('users').insert(
+      return userAuthReddit.deauth()
+      .then(r.table('users').insert(
         {name: data.name, lastLogin: r.now()},
-        {conflict: 'update'}).run(conn).then(function() {
-          return userAuthReddit.deauth();
-        }).then(function() {
-          if (data.name == botName) {
-            req.session.bot = crypto.randomBytes(64).toString('hex');
-            res.redirect(botAuthReddit.getExplicitAuthUrl(req.session.bot));
-          } else {
-            req.session.username = data.name;
-            res.redirect('/');
-          }
-        });
+        {conflict: 'update'}).run(conn))
+      .then(function() {
+        if (data.name == botName) {
+          req.session.bot = crypto.randomBytes(64).toString('hex');
+          res.redirect(botAuthReddit.getExplicitAuthUrl(req.session.bot));
+        } else {
+          req.session.username = data.name;
+          res.redirect('/');
+        }
+      });
     });
   }
 
   function authBot(req, res) {
     botAuthReddit.auth(req.query.code).then(function (refreshToken) {
-      botAuthReddit.deauth().then(function () {
-        return r.table('users').get(botName)
-          .update({refreshToken: refreshToken}).run(conn);
-      }).then(function () {
+      return botAuthReddit.deauth()
+      .then(r.table('users').get(botName)
+        .update({refreshToken: refreshToken}).run(conn))
+      .then(function () {
         // "log out" and redirect to the index
         // so we can log in as ourselves
         delete res.session.username;
